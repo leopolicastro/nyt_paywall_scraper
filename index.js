2df291846330a2841aa28b60e5f8cb9ac1f536c9
leopolicastro <lpolicastro@pm.me>
2020-03-01 13:23:08 -0500

    edit index.js,...again

--- index.js
+++ index.js
@@ -29,15 +29,6 @@ app.get("/", function(req, res) {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", (request, response) => {
-    response.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`API listening on port ${port}...`);
