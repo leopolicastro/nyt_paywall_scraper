cc460f940c76d160d0f664ee04b9822a6a6774af
leopolicastro <lpolicastro@pm.me>
2020-03-01 13:28:18 -0500

    dokku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const cheerio = require("cheerio");
+const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -30,3 +31,14 @@ app.get("/", function(req, res) {
 app.listen("8081");
 console.log("NYT Started on Port 8081");
 exports = module.exports = app;
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "views")));
+  // Handle React routing, return all requests to React app
+}
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`API listening on port ${port}...`);
+});
